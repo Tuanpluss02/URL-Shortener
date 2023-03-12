0358e401bb5ea074128401338193dc66459bc634
Tuân Đỗ <82562559+Tuanpluss02@users.noreply.github.com>
2023-03-12 06:42:20 +0000

    Pending changes exported from your codespace

---
+++ config.py
@@ -0,0 +1,4 @@
+MONGODB_URL = ""
+MONGODB_NAME = "URL_Shortener"
+SHORT_URL_LENGTH = 10
+BASE_SHORT_URL = "http://localhost:8000/"
--- main.py
+++ main.py
@@ -1,10 +1,65 @@
+import logging
+import pymongo
+import json
 from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv()
+from fastapi.responses import RedirectResponse
+from starlette.middleware.cors import CORSMiddleware
+from mongodb import close_mongo_connection, connect_to_mongo, get_nosql_db
+from config import MONGODB_NAME, BASE_SHORT_URL
+
 
 app = FastAPI()
+
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:3000",
+#     "http://localhost:3000",
+# ]
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # can alter with time
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+async def startup_event():
+    await connect_to_mongo()
+    client = await get_nosql_db()
+    db = client[MONGODB_NAME]
+    if "url_collection" not in db.list_collection_names():
+        try:
+            db.create_collection("url_collection")
+        except pymongo.errors.CollectionInvalid as error:
+            logging.warning(error)
+    try:
+        url_collection = db.url_collection
+        url_collection.create_index(
+            "shortname", shortname="shortname", unique=True)
+    except pymongo.errors.CollectionInvalid as error:
+        logging.warning(error)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await close_mongo_connection()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    "Root endpoint"
+    return RedirectResponse(url="https://stormx.software")
+
+
+@app.post("/shorten")
+async def shorten_url(long_url: str, short_name: str):
+    short_url = BASE_SHORT_URL + short_name
+    db = await get_nosql_db()
+    url_collection = db.url_collection
+    url = await url_collection.find_one({"shortname": short_name})
